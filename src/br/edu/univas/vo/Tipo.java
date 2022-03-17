943a322ab36e4aa7280cf782e9b064c21be6cc86
RonaldSilva1427 <ronald.psilva14@gmail.com>
2022-03-17 00:47:47 -0300

    Commit Test

---
+++ src/br/edu/univas/vo/Tipo.java
@@ -0,0 +1,15 @@
+package br.edu.univas.vo;
+
+public class Tipo {
+
+    private String filme;
+    private String serie;
+
+    public String getFilme() {return filme;}
+
+    public void setFilme(String filme) {this.filme = filme;}
+
+    public String getSerie() {return serie;}
+
+    public void setSerie(String serie) {this.serie = serie;}
+}
